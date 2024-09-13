181e80d9b2a6b045a4308a073c893a5fbea69d4e
ajuroshan <ajuaju0483@gmail.com>
2024-09-14 00:04:37 +0530

    feat: added the auth app

---
+++ authentication/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+@csrf_exempt
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({"error": "Invalid Username or password"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(request, username=user.username, password=password)
+
+    if user is not None:
+        login(request, user)
+        # Return user data after successful login
+        return Response({
+            "message": "Logged in successfully.",
+            "user": {
+                "username": user.username,
+                "id": user.id
+            }
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Invalid Username or password"}, status=status.HTTP_400_BAD_REQUEST)
+@csrf_exempt
+@api_view(['POST'])
+def signup_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already in use"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+
+    return Response({"message": "Signup successful."}, status=status.HTTP_201_CREATED)
+@csrf_exempt
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return Response({"message": "Logged out successfully."}, status=status.HTTP_200_OK)
