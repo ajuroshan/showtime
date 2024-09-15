5bf02d1594457c60ff390964c55ceb6cd00e8278
ajuroshan <ajuaju0483@gmail.com>
2024-09-15 17:10:46 +0530

    fix: auth

--- api/views.py
+++ api/views.py
@@ -21,7 +21,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         # Set the user to the logged-in user
-        serializer.save(user=self.request.user)
+        serializer.save()
 
 
 class ShowSearchView(generics.ListAPIView):
